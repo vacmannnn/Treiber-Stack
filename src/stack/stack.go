28ee59f173736836807e83cd6e8fd321e3e20d33
Alexandr <opera9876@rambler.ru>
2024-02-17 20:36:15 +0300

    feat: implement push on Treiber stack

--- src/stack/stack.go
+++ src/stack/stack.go
@@ -1,18 +1,38 @@
 package stack
 
-import "fmt"
+import (
+    "sync/atomic"
+    "unsafe"
+)
 
-type stack struct {
+type node struct {
+    value int
+    next  unsafe.Pointer
 }
 
-func Push() {
-    fmt.Println("Hello Treiber!")
+type Stack struct {
+    head unsafe.Pointer
 }
 
-func Pop() {
+func NewStack() Stack {
+    return Stack{}
+}
+
+func (s Stack) Push(value int) {
+    newNode := &node{value: value}
+    for {
+        head := atomic.LoadPointer(&s.head)
+        newNode.next = head
+        if atomic.CompareAndSwapPointer(&head, newNode.next, unsafe.Pointer(newNode)) {
+            return
+        }
+    }
+}
+
+func (s Stack) Pop() {
 
 }
 
-func PrintStack() {
+func (s Stack) Stringer() {
 
 }
