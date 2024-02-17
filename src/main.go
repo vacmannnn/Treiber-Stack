b55a0747da1bbc2984c1da2e25e38d75528804e7
Alexandr <opera9876@rambler.ru>
2024-02-17 22:03:07 +0300

    feat: simple test on concurrent and non-concurrent push

--- src/main.go
+++ src/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+    "Treiber-Stack/stack"
     "fmt"
+    "runtime"
+    "sync"
+    "time"
 )
 
+const goroutineNumber = 50000000
+
 func main() {
-    fmt.Println("Hello world!")
+    fmt.Printf("GOMAXPROCS=%d\n", runtime.GOMAXPROCS(0))
+    myStack := stack.NewStack()
+    fmt.Println("Single thread")
+    now := time.Now()
+    for i := range goroutineNumber {
+        myStack.Push(i)
+    }
+    fmt.Println(time.Now().Sub(now).Truncate(time.Millisecond))
+    fmt.Println(&myStack)
+
+    fmt.Println()
+    myStack = stack.NewStack()
+    wg := sync.WaitGroup{}
+    wg.Add(goroutineNumber)
+    fmt.Println("Concurrent/parallel")
+    now = time.Now()
+    for i := range goroutineNumber {
+        go func(i int) {
+            myStack.Push(i)
+            wg.Done()
+        }(i)
+    }
+    wg.Wait()
+    fmt.Println(time.Now().Sub(now).Truncate(time.Millisecond))
+    fmt.Println(&myStack)
+    fmt.Println("\n\n\n")
 }
