2ad01419428765de590a045a47784b14c4910545
Alexander <opera9876@rambler.ru>
2024-03-31 00:24:48 +0300

    feat: benchmark for random push and pop

--- bench.go
+++ bench.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/vacmannnn/Treiber-Stack/backoffStack"
 	"github.com/vacmannnn/Treiber-Stack/stack"
 	"runtime"
 	"sync"
@@ -55,4 +56,39 @@ func BenchNotManyGoroutines() {
 		wg.Wait()
 		fmt.Println(numOfGoroutines, "goroutines:", time.Since(startTime).Truncate(time.Millisecond))
 	}
+}
+
+var threadsNum = [...]int{2, 4, 6, 8, 10, 12}
+
+func BenchPopAndPush() {
+	cStack := backoffStack.NewStack[int]()
+	abc := 24_000_000
+	// bStack := backoffStack.NewStack[int]()
+	for i := 0; i < abc/24; i++ {
+		cStack.Push(123)
+	}
+	fmt.Println(&cStack)
+	wg := sync.WaitGroup{}
+	wg.Add(8)
+	now := time.Now()
+	for i := 0; i < 4; i++ {
+		go func() {
+			for j := 0; j < abc/4; j++ {
+				cStack.Push(123)
+			}
+			wg.Done()
+		}()
+	}
+	fmt.Println(&cStack)
+	for i := 0; i < 4; i++ {
+		go func() {
+			for j := 0; j < abc/4; j++ {
+				cStack.Pop()
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("time:", time.Since(now).Truncate(time.Millisecond))
+	fmt.Println(&cStack)
 }
